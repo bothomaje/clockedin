54dccd0954bbcad2e4f854df0ec99c9584841a96
Botho Maje <bothomaje@gmail.com>
2026-08-27 22:09:57 +0200

    fix: update dashboard to match new job model

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,14 +1,18 @@
 import { Component, inject } from '@angular/core';
 import { JobService } from '../../services/job-service';
 import { Job } from '../../models/job.model';
+import { UserService } from '../../services/user-service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
-  imports: [],
+  imports: [AsyncPipe],
   selector: 'app-dashboard',
   // styleUrl: './dashboard.scss',
   templateUrl: './dashboard.html',
 })
 export class Dashboard {
   private jobService = inject(JobService);
-  jobs: Job[] = this.jobService.getJobs();
+  private userService = inject(UserService);
+
+  jobs = this.jobService.getJobs();
 }

0642b30128be7e36180274d22f754da81e16978e
Botho Maje <bothomaje@gmail.com>
2026-08-27 22:31:18 +0200

    feat: add functionality to save new job listing

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -3,9 +3,10 @@ import { JobService } from '../../services/job-service';
 import { Job } from '../../models/job.model';
 import { UserService } from '../../services/user-service';
 import { AsyncPipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, FormsModule],
   selector: 'app-dashboard',
   // styleUrl: './dashboard.scss',
   templateUrl: './dashboard.html',
@@ -14,5 +15,26 @@ export class Dashboard {
   private jobService = inject(JobService);
   private userService = inject(UserService);
 
+  editedJob?: Job;
+
   jobs = this.jobService.getJobs();
+
+  beginJobEdit() {
+    this.editedJob = {
+      company: '',
+      role: '',
+      jobDescription: '',
+      jobUpdates: [],
+    };
+  }
+
+  cancelJobEdit() {
+    this.editedJob = undefined;
+  }
+
+  saveJob() {
+    this.jobService.addJob(this.editedJob!);
+    this.editedJob = undefined;
+    this.jobs = this.jobService.getJobs();
+  }
 }
